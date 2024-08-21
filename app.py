bd32d836e9e766b2e07695fc2d78468f858b9e85
busycalibrating <19653980+busycalibrating@users.noreply.github.com>
2024-08-21 17:53:58 -0400

    basic working

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import argparse
 import os
 import openai
 from openai import OpenAI
-
 import streamlit as st
 
 
@@ -15,7 +14,6 @@ LOG_LEVELS = {logging.DEBUG: 'Debug', logging.INFO: 'Info', logging.WARN: "Warni
 def parse_args():
     parser = argparse.ArgumentParser(prog='Chat Client', description='Interfaces with vLLM on the Mila Cluster', epilog='Dont fuck it up')
     parser.add_argument("--url", type=str, default="http://localhost:8899/v1", help="URL and port on the Mila cluster")
-    parser.add_argument("--model", type=str, default="/network/weights/llama.var/llama2/Llama-2-7b-chat-hf", help="Default model to select")
     try:
         args = parser.parse_args()
         return args
@@ -43,12 +41,11 @@ with st.sidebar:
     #   [?] reset chat  -> f5 lol
     #   [ ] control prompt template
     #       [ ] be able to toggle between raw inputs vs templated response
-    # mode = st.selectbox('Chat mode', options=CHAT_MODES)
-    # logger.info("Selected chat mode: %s", mode)
     log_level = st.selectbox('Logging verbosity', options=list(LOG_LEVELS.keys()), format_func=lambda x: LOG_LEVELS[x])
     logger.setLevel(level=log_level)
 
     model = st.selectbox('Model', all_model_ids)
+    st.session_state['openai_model'] = model
     logger.info("Selected model: %s", model)
 
     temperature = st.slider("Temperature", min_value=0., max_value=1., value=0.7)
@@ -59,15 +56,10 @@ with st.sidebar:
 
     system_prompt = st.text_area("System Prompt", value=None)
     logger.info("System prompt: %s", system_prompt)
-
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = args.model  # "gpt-3.5-turbo"
+    st.session_state['system_prompt'] = system_prompt
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    if system_prompt is not None:
-        st.session_state.messages.append({"role": "system", "content": system_prompt})
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -78,27 +70,22 @@ if prompt := st.chat_input("Chat conversation"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    logger.debug(st.session_state.messages)
-
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
 
+        messages = [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]
+        if st.session_state.system_prompt is not None:
+            messages = [{"role": "system", "content": st.session_state.system_prompt}] + messages
+        logger.debug("Messages: %s", messages)
+
         chat_completion = client.chat.completions.create(
             model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
+            messages=messages,
             temperature=temperature,
             stream=True,
             max_tokens=max_tokens
         )
         logger.debug(chat_completion)
         response = st.write_stream(chat_completion)
-        # for response in chat_completion:
-        #     logger.debug(response)
-        #     full_response += response.choices[0].delta.get("content", "")
-        #     message_placeholder.markdown(full_response + "▌")
-        # message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": response})
