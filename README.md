487c00c7afc18cfa9158b4df6339e4f302f2de90
busycalibrating <19653980+busycalibrating@users.noreply.github.com>
2024-08-21 16:58:26 -0400

    update to newer/more general chat app

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# streamlit-llm-app
+# LLM Streamlit App
+
+A minimal Streamlit app to interface with vLLM and arbitrary (supported) models.
+
+## Installation
+
+Follow the instructions to setup vLLM (make sure you use the right CUDA module if necessary):
+
+```bash
+module load cuda/12.1.1
+
+# (Recommended) Create a new conda environment.
+conda create -n vllm python=3.10 -y
+conda activate vllm
+
+# Install vLLM with CUDA 12.1.
+pip install vllm
+```
+
+Then install the requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+1. Serve the LLM with vLLM on the cluster:
+
+```bash
+# optionally append the jinja template if needed
+vllm serve /network/weights/llama.var/llama2/Llama-2-7b-chat-hf/ --port 8899 --chat-template ./chat_templates/llama2_v2.jinja
+```
+
+2. Local port forwarding if necessary:
+
+```bash
+ssh -t -t mila -L 8899:localhost:8899 ssh USER@NODE -L 8899:localhost:8899
+```
+
+3. Profit
--- app.py
+++ app.py
@@ -1,200 +1,104 @@
-import openai
-import streamlit as st
+import logging
 import argparse
 import os
-import logging
+import openai
+from openai import OpenAI
 
-from utils import LLMAttacksPrompt
+import streamlit as st
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 LOG_LEVELS = {logging.DEBUG: 'Debug', logging.INFO: 'Info', logging.WARN: "Warning"}
 
-CHAT_MODES = ['chat', 'raw', 'raw chat', 'fastchat raw']
 
-st.title("Chat Client")
+def parse_args():
+    parser = argparse.ArgumentParser(prog='Chat Client', description='Interfaces with vLLM on the Mila Cluster', epilog='Dont fuck it up')
+    parser.add_argument("--url", type=str, default="http://localhost:8899/v1", help="URL and port on the Mila cluster")
+    parser.add_argument("--model", type=str, default="/network/weights/llama.var/llama2/Llama-2-7b-chat-hf", help="Default model to select")
+    try:
+        args = parser.parse_args()
+        return args
+    except SystemExit as e:
+        # This exception will be raised if --help or invalid command line arguments
+        # are used. Currently streamlit prevents the program from exiting normally
+        # so we have to do a hard exit.
+        os._exit(e.code)
 
-parser = argparse.ArgumentParser(prog='Chat Client', description='Interfaces with vLLM on the Mila Cluster', epilog='Dont fuck it up')
-parser.add_argument("--url", type=str, default="http://localhost:8899/v1", help="URL and port on the Mila cluster")
-parser.add_argument("--model", type=str, default="/network/scratch/d/david-a.dobre/llms/Llama-2-7b-chat-hf/", help="Default model to select")
+args = parse_args()
 
-try:
-    args = parser.parse_args()
-except SystemExit as e:
-    # This exception will be raised if --help or invalid command line arguments
-    # are used. Currently streamlit prevents the program from exiting normally
-    # so we have to do a hard exit.
-    os._exit(e.code)
-
-# openai.api_key = st.secrets["OPENAI_API_KEY"]
-openai.api_key = "EMPTY"
-openai.api_base = args.url
+st.title("ChatGPT-like client")
+client = OpenAI(api_key="EMPTY", base_url=args.url)
 
 # List models API
-all_model_ids = [m['id'] for m in openai.Model.list()["data"]]
-print("Models:", all_model_ids)
-# model = all_model_ids
-
+all_model_ids = [model.id for model in client.models.list()]
+logger.info(all_model_ids)
 
 with st.sidebar:
     # TODO:
     #   [x] select model
-    #   [x] control log level
     #   [x] query hparams 
     #       [x] Max tokens
     #       [x] temperature   
-    #   [ ] reset chat  -> f5 lol
+    #   [?] reset chat  -> f5 lol
     #   [ ] control prompt template
     #       [ ] be able to toggle between raw inputs vs templated response
+    # mode = st.selectbox('Chat mode', options=CHAT_MODES)
+    # logger.info("Selected chat mode: %s", mode)
     log_level = st.selectbox('Logging verbosity', options=list(LOG_LEVELS.keys()), format_func=lambda x: LOG_LEVELS[x])
     logger.setLevel(level=log_level)
 
-    mode = st.selectbox('Chat mode', options=CHAT_MODES)
-    logger.debug(f"Selected chat mode: {mode}")
-
     model = st.selectbox('Model', all_model_ids)
-    logger.debug(f"Selected model: {model}")
+    logger.info("Selected model: %s", model)
 
     temperature = st.slider("Temperature", min_value=0., max_value=1., value=0.7)
-    logger.debug(f"Temperature: {temperature}")
+    logger.info("Temperature: %s", temperature)
 
     max_tokens = st.number_input("Max num. tokens", min_value=0, value=100)
-    logger.debug(f"Max num. tokens:: {max_tokens}")
+    logger.info("Max num. tokens: %s", max_tokens)
 
-    if "raw" in mode:
-        n_completions = st.number_input("Number of completions per prompt", min_value=1, value=1)
-        logprobs = st.number_input("log probs", min_value=0, max_value=5, value=0)
+    system_prompt = st.text_area("System Prompt", value=None)
+    logger.info("System prompt: %s", system_prompt)
 
-    if "fastchat" in mode:
-        goal = st.text_area("Goal text")
-        control = st.text_area("Control text")
-        target = st.text_area("Target text")
-        submit = st.button("Submit")
-# Initialize
+
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = args.model
+    st.session_state["openai_model"] = args.model  # "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    if system_prompt is not None:
+        st.session_state.messages.append({"role": "system", "content": system_prompt})
 
-# Display messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Submit messages?
+if prompt := st.chat_input("Chat conversation"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-# > curl http://localhost:8899/v1/completions -H "Content-Type: application/json" -d '{
-#           "model": "/network/scratch/d/david-a.dobre/llms/Llama-2-7b-chat-hf/",
-#           "prompt": "San Francisco is a",
-#           "max_tokens": 200,
-#           "temperature": 0
-#       }'
+    logger.debug(st.session_state.messages)
 
-if mode == "chat":
-    if prompt := st.chat_input("Chat conversation"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        
-        logger.debug(st.session_state.messages)
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
 
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-
-            chat_completion = openai.ChatCompletion.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                temperature=temperature,
-                stream=True,
-                max_tokens=max_tokens,
-            )
-            logger.debug(chat_completion)
-
-            for response in chat_completion:
-                logger.debug(response)
-                full_response += response.choices[0].delta.get("content", "")
-                message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-elif mode == "raw":
-    # Reset chat
-    if len(st.session_state.messages) != 0:
-        st.session_state.messages = []
-
-    if prompt := st.chat_input("Raw conversation"):
-
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-
-            completion = openai.Completion.create(
-                model=st.session_state["openai_model"],
-                prompt=st.session_state.messages[-1]['content'],
-                echo=False,
-                n=n_completions,
-                logprobs=logprobs,
-                temperature=temperature,
-                stream=True,
-                max_tokens=max_tokens,
-            )
-
-            for response in completion:
-                logger.debug(response)
-                full_response += response.choices[0].get("text", "")
-                message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response)
-
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-elif mode == "fastchat raw":
-    conv_template = LLMAttacksPrompt('llama-2')
-    logger.info(f"Conv template: {conv_template.name}")
-    logger.info(f"Goal: {goal}; Control: {control}; Target: {target}")
-
-    prompt = conv_template.llm_attack_prompt(goal=goal, control=control, target=target)
-    logger.info(prompt)
-
-    st.markdown(prompt)
-
-    st.divider()
-
-    if submit:
-        st.session_state.messages = []
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-
-            completion = openai.Completion.create(
-                model=st.session_state["openai_model"],
-                prompt=prompt,
-                echo=False,
-                n=n_completions,
-                logprobs=logprobs,
-                temperature=temperature,
-                stream=True,
-                max_tokens=max_tokens,
-            )
-
-            for response in completion:
-                logger.debug(response)
-                full_response += response.choices[0].get("text", "")
-                message_placeholder.markdown(full_response + "▌")
-
-            message_placeholder.markdown(full_response)
-
-            st.session_state.messages.append({"role": "assistant", "content": full_response})
-            
-
-elif mode == "raw chat":
-    st.write("Not implemented")
+        chat_completion = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            temperature=temperature,
+            stream=True,
+            max_tokens=max_tokens
+        )
+        logger.debug(chat_completion)
+        response = st.write_stream(chat_completion)
+        # for response in chat_completion:
+        #     logger.debug(response)
+        #     full_response += response.choices[0].delta.get("content", "")
+        #     message_placeholder.markdown(full_response + "▌")
+        # message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
